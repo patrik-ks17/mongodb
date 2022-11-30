e1a8ba58dee36d95dfe28bc9214f3b8c11af0087
KasaPatrik <kasaip02@gmail.com>
2022-11-30 10:37:48 +0100

    asd

--- index.js
+++ index.js
@@ -78,15 +78,56 @@ app.delete('/cars/:id',  (req, res) => {
 })
 
 
-// Edit specified car
-app.put('/cars/:id',  (req, res) => { 
+const bodyParser = require('body-parser');
 
+// Edit specified car
+app.put('/cars/:id', bodyParser.json(), (req, res) => { 
+	const updatedCar = {
+		name: body.name,
+		licenseNumber: body.licenseNumber,
+		hourlyRate: body.hourlyRate,
+	};
+
+	const id = getId(req.params.id);
+	if (!id) {
+		res.send({error: 'Invalid id'});
+		return;
+	}
+	const client = getClient();
+	client.connect(async err => {
+		const collection = client.db("taxi_app").collection("cars");
+		const result = await collection.updateOne({_id: id}, {$set: updatedCar});
+		if (!result.modifiedCount) {
+			res.send({error: 'Not found'});
+			return;
+		}
+		res.send(updatedCar);
+		client.close();
+	});
+	
 })
 
 
 // Add new car
-app.post('/cars',  (req, res) => { 
+app.post('/cars', bodyParser.json(), (req, res) => { 
+	const newCar = {
+		name: body.name,
+		licenseNumber: body.licenseNumber,
+		hourlyRate: body.hourlyRate,
+		trips: []
+	};
 
+	const client = getClient();
+	client.connect(async err => {
+		const collection = client.db("taxi_app").collection("cars");
+		const result = await collection.insertOne(newCar);
+		if (!result.insertedCount) {
+			res.send({error: 'Insert error'});
+			return;
+		}
+		res.send(newCar);
+		client.close();
+	});
 })
 
 
