bb3c3c433c7f19438675fe0862eb87d229318cf7
KasaPatrik <kasaip02@gmail.com>
2022-11-30 12:13:50 +0100

    f10

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const ObjectId = require("mongodb").ObjectId;
+const bodyParser = require('body-parser');
 
 
 function getClient() {
@@ -78,14 +79,12 @@ app.delete('/cars/:id',  (req, res) => {
 })
 
 
-const bodyParser = require('body-parser');
-
 // Edit specified car
 app.put('/cars/:id', bodyParser.json(), (req, res) => { 
 	const updatedCar = {
-		name: body.name,
-		licenseNumber: body.licenseNumber,
-		hourlyRate: body.hourlyRate,
+		name: req.body.name,
+		licenseNumber: req.body.licenseNumber,
+		hourlyRate: req.body.hourlyRate,
 	};
 
 	const id = getId(req.params.id);
@@ -111,9 +110,9 @@ app.put('/cars/:id', bodyParser.json(), (req, res) => {
 // Add new car
 app.post('/cars', bodyParser.json(), (req, res) => { 
 	const newCar = {
-		name: body.name,
-		licenseNumber: body.licenseNumber,
-		hourlyRate: body.hourlyRate,
+		name: req.body.name,
+		licenseNumber: req.body.licenseNumber,
+		hourlyRate: req.body.hourlyRate,
 		trips: []
 	};
 
@@ -121,7 +120,7 @@ app.post('/cars', bodyParser.json(), (req, res) => {
 	client.connect(async err => {
 		const collection = client.db("taxi_app").collection("cars");
 		const result = await collection.insertOne(newCar);
-		if (!result.insertedCount) {
+		if (!result.insertedId) {
 			res.send({error: 'Insert error'});
 			return;
 		}
